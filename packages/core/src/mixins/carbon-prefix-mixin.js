5554bb884118abe71355b6b361c9db176535cbe3
Lee Chase <lee.chase@uk.ibm.com>
2020-06-25 13:25:02 +0100

    chore: add combo box prefix

--- packages/core/src/mixins/carbon-prefix-mixin.js
+++ packages/core/src/mixins/carbon-prefix-mixin.js
@@ -4,4 +4,21 @@ export default {
   created() {
     this.carbonPrefix = carbonSettings.prefix;
   },
+  computed: {
+    toClassObject() {
+      // Vue form "{ 'yyy': test, 'yyy', test }"
+      // Expected form "['yyy', test, 'yyy', test]"
+
+      // 'xxx' can be a template literal however
+      // In JSON Objects 'yyy' cannot be a template literal
+
+      return pairs => {
+        const obj = {};
+        for (let i = 0; i < pairs.length; i += 2) {
+          obj[pairs[i]] = pairs[i + 1];
+        }
+        return obj;
+      };
+    },
+  },
 };
