b99c510bb4dc2227b7c832fb590b6f75876f45f7
Lee Chase <lee.chase@uk.ibm.com>
2020-12-18 16:58:25 +0000

    feat: add  overflow indicators snippet

--- packages/core/src/components/cv-code-snippet/_cv-code-snippet-oneline.vue
+++ packages/core/src/components/cv-code-snippet/_cv-code-snippet-oneline.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="cv-code-snippet-oneline" :class="[`${carbonPrefix}--snippet`, `${carbonPrefix}--snippet--single`]">
-    <div :class="`${carbonPrefix}--snippet-container`">
+    <div :class="`${carbonPrefix}--snippet-container`" ref="container" @scroll="measure">
       <pre>
         <slot></slot>
       </pre>
+      <resize-observer @notify="handleResize" />
     </div>
+    <div v-if="hasLeftOverflow" :class="`${carbonPrefix}--snippet__overflow-indicator--left`" />
+    <div v-if="hasRightOverflow" :class="`${carbonPrefix}--snippet__overflow-indicator--right`" />
+
     <cv-feedback-button
       v-if="!hideCopyButton"
       :feedback="copyFeedback"
@@ -20,6 +24,7 @@
 import { carbonPrefixMixin } from '../../mixins';
 import CvFeedbackButton from '../cv-feedback-button/_cv-feedback-button';
 import Copy16 from '@carbon/icons-vue/es/copy/16';
+import ResizeObserver from 'vue-resize/src/components/ResizeObserver.vue';
 
 export default {
   name: 'CvCodeSnippetOneline',
@@ -27,11 +32,38 @@ export default {
   components: {
     CvFeedbackButton,
     Copy16,
+    ResizeObserver,
   },
   props: {
     copyFeedback: String,
     feedbackAriaLabel: String,
     hideCopyButton: Boolean,
+  },
+  data() {
+    return {
+      hasLeftOverflow: false,
+      hasRightOverflow: false,
+    };
+  },
+  mounted() {
+    this.measure();
+  },
+  created() {
+    window.addEventListener('resize', this.measure);
+  },
+  destroyed() {
+    window.removeEventListener('resize', this.measure);
+  },
+  methods: {
+    handleResize() {
+      this.measure();
+    },
+    measure() {
+      const el = this.$refs.container;
+
+      this.hasLeftOverflow = el.scrollLeft > 0;
+      this.hasRightOverflow = el.scrollWidth - el.clientWidth - el.scrollLeft > 0;
+    },
   },
 };
 </script>
